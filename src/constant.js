f3ef97bae8ec4b632103b2d74fb98d69b7797ac2
rajtirole <rtech23456@gmail.com>
2024-04-15 11:28:55 +0530

    job posting

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const Backend_URL='http://localhost:5400/api/v1'
+export const Backend_URL='https://job-posting-app-bakend.onrender.com/api/v1'
 export const FRONTEND_URL='http://localhost:3000'
