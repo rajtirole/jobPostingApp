a5f460b5af208fa2eedc60c7b61767ab771fb9bd
rajtirole <rtech23456@gmail.com>
2024-04-15 18:39:13 +0530

    job posting

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const Backend_URL='http://localhost:5400/api/v1'
-export const FRONTEND_URL='http://localhost:3000'
-// export const Backend_URL='https://job-posting-app-bakend.onrender.com/api/v1'
-// export const FRONTEND_URL='https://jobpostingapp-1.onrender.com'
+// export const Backend_URL='http://localhost:5400/api/v1'
+// export const FRONTEND_URL='http://localhost:3000'
+export const Backend_URL='https://job-posting-app-bakend.onrender.com/api/v1'
+export const FRONTEND_URL='https://jobpostingapp-1.onrender.com'
